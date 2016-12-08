cf98962a726f20a9c799975c5ce32ec6c4f10c07
Miguel Uy <yuleugim@gmail.com>
2016-12-08 15:27:37 -0500

    Reorganized file structure to packages

--- src/main/java/ubay/application/Ubay.java
+++ src/main/java/ubay/application/Ubay.java
@@ -1,71 +1,35 @@
 package ubay.application;
 
-import spark.*;
-import spark.template.velocity.*;
-import ubay.route.LoginRoute;
-
+import ubay.route.*;
 import java.sql.*;
-import java.util.*;
 import static spark.Spark.*;
 
 public class Ubay {
 
-    public static Connection con;
-
     public static void main(String [] args) {
 
-//        try {
-//            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/uBay", "root", "1234");
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
+        connectDatabase();
 
         exception(Exception.class, (e, req, res) -> e.printStackTrace()); // print all exceptions
         staticFiles.location("/public");
         port(9999);
 
-        // Render main UI
-        get("/", (req, res) -> renderGUI(req));
-
         initializeRoutes();
 
     }
 
-    public static String parseLogin(Request req) {
-
-        String email = req.queryParams("email");
-        String password = req.queryParams("password");
-
-        try {
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT card FROM account WHERE email = '"+email+"'" +" AND "+ "password = '"+password+"'");
-            rs.next();
-            String cnum = rs.getString("card");
-            System.out.println(cnum);
-        } catch (SQLException exc) {
-            System.out.println("Invalid Login"); }
-
-        return renderTemplate("velocity/home.vm", new HashMap()); }
-
-    public static String renderGUI(Request req) {
-        String statusStr = req.queryParams("status");
-        Map<String, Object> model = new HashMap<>();
-        if ("true".equals(req.queryParams("ic-request"))) {
-            return renderTemplate("velocity/todoList.vm", model);
-        }
-        return renderTemplate("velocity/index.vm", model);
-    }
-
-    public static String renderLoginTemplate(Request req) {
-        Map<String, Object> model = new HashMap<>();
-        return renderTemplate("velocity/login.vm", model);
-    }
-
-    public static String renderTemplate(String template, Map model) {
-        return new VelocityTemplateEngine().render(new ModelAndView(model, template));
-    }
+    public static Connection con;
 
     private static void initializeRoutes() {
+        new IndexRoute();
         new LoginRoute();
+    }
+
+    private static void connectDatabase() {
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/uBay", "root", "1234");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/ubay/route/LoginRoute.java
+++ src/main/java/ubay/route/LoginRoute.java
@@ -1,13 +1,46 @@
 package ubay.route;
 
-import static spark.Spark.*;
+import spark.Request;
+
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.put;
 import static ubay.application.Ubay.*;
 
-public class LoginRoute {
+public class LoginRoute extends TemplateRenderer {
 
     public LoginRoute() {
-        get("/login", (req, res) -> renderLoginTemplate(req));
-        put("/login2", (req, res) -> parseLogin(req));
+        get("/login-template", (req, res) -> renderLoginTemplate(req));
+        put("/login-data", (req, res) -> parseLoginData(req));
+    }
+
+    private String renderLoginTemplate(Request req) {
+        Map<String, Object> model = new HashMap<>();
+        return renderTemplate("velocity/login.vm", model);
+    }
+
+    private String parseLoginData(Request req) {
+
+        String email = req.queryParams("email");
+        String password = req.queryParams("password");
+
+        try {
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT card FROM account WHERE email = '"+email+"'" +" AND "+ "password = '"+password+"'");
+            rs.next();
+            String cnum = rs.getString("card");
+            System.out.println(cnum);
+        } catch (SQLException exc) {
+            System.out.println("Invalid Login");
+        }
+
+        return renderTemplate("velocity/home.vm", new HashMap());
     }
 
 }
