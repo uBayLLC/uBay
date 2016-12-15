48ab049be35711713f906636cbfe9b8ff5ae18d0
Kyle Degen <kcdegen@gmail.com>
2016-12-14 19:46:32 -0500

    Item pages show date and time seperatly and formatted correctly

--- src/main/java/ubay/model/Auction.java
+++ src/main/java/ubay/model/Auction.java
@@ -2,7 +2,12 @@ package ubay.model;
 
 import lombok.*;
 
-import java.util.Date;
+import java.util.*;
+import java.text.*;
+import java.text.FieldPosition;
+import java.text.ParsePosition;
+
+
 
 @Data
 @RequiredArgsConstructor
@@ -14,4 +19,15 @@ public class Auction {
     @NonNull private int buyOutPrice;
     @NonNull private Bid currentBid;
 
+
+    public String getEndDate() {
+        DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+        String formattedDate = dateFormat.format(this.getEndDateTime());
+        return formattedDate; }
+
+    public String getEndTime() {
+        DateFormat timeFormat = new SimpleDateFormat("HH:mm");
+        String formattedTime = timeFormat.format(this.getEndDateTime());
+        return formattedTime; }
 }
+
