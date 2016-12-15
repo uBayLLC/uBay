1f5d13b3b3643d75d15ad13cdd0f685be75fe1e5
Miguel Uy <yuleugim@gmail.com>
2016-12-15 01:42:31 -0500

    Bug fixes

--- src/main/java/ubay/route/SearchRoute.java
+++ src/main/java/ubay/route/SearchRoute.java
@@ -17,7 +17,6 @@ import static ubay.route.TemplateRenderer.renderTemplate;
 
 public class SearchRoute {
 
-
     public SearchRoute()
     {
         post("/search/data", (req, res) -> parseSearchData(req));
@@ -40,20 +39,16 @@ public class SearchRoute {
         List<Item> items = new ArrayList<>();
         try {
 
-             if(!itemName.equalsIgnoreCase("")){
-
-                preparedStatement = con.prepareStatement("Select * From Item Where name like '" + itemName +"%';");
+            if (!itemName.equalsIgnoreCase("") && !filter.equalsIgnoreCase("")) {
+                preparedStatement = con.prepareStatement("SELECT item.item_id, item.name, item.description, item.photo, item.seller_id, item.tag_id FROM item JOIN tag ON tag.tag_id = item.tag_id WHERE tag.name LIKE '%" + filter + "%' AND WHERE item.name = '%" + itemName + "%'");
+                result = preparedStatement.executeQuery();
+            } else if(!itemName.equalsIgnoreCase("")){
+                preparedStatement = con.prepareStatement("SELECT * FROM item WHERE name LIKE '%" + itemName +"%'");
+                result = preparedStatement.executeQuery();
+            } else if(!filter.equalsIgnoreCase("")) {
+                preparedStatement = con.prepareStatement("SELECT item.item_id, item.name, item.description, item.photo, item.seller_id, item.tag_id FROM item JOIN tag ON tag.tag_id = item.tag_id WHERE tag.name LIKE '%" + filter + "%'");
                 result = preparedStatement.executeQuery();
             }
-            if(!filter.equalsIgnoreCase(""))
-            {
-                preparedStatement = con.prepareStatement("Select item.item_id, item.name, item.description, item.photo, item.seller_id, item.tag_id From Item Join tag t on t.tag_id=item.tag_id Where t.name like '" + filter + "%';");
-                result = preparedStatement.executeQuery();
-            }
-
-
-
-
 
             while(result.next()) {
                 Item item = new Item(
@@ -65,10 +60,8 @@ public class SearchRoute {
                         result.getInt("tag_id")
                 );
                 items.add(item);
-
             }
         } catch(SQLException e) {
-
             e.printStackTrace();
         } finally {
             if (result != null) {
@@ -80,7 +73,6 @@ public class SearchRoute {
                 catch (SQLException e) { e.printStackTrace(); }
             }
         }
-
 
         return items;
     }
